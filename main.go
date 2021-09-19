d782fff264d78d7d03b36b556d01edbb04ddb558
Michael Endsley <michael.endsley@pearson.com>
2021-09-19 17:48:01 -0500

    Rename struct Comparison to Request as that makes it a little more clear as to what it's for. Create CommitMessage struct to hold only the types we're looking to extract from github.CommitComparison.
    Loop over paginated result sets from GitHub to build slice of
    CommitMessages, limiting the number of pages we request to the amount
    necessary to serve our full result set.
    
    Temporarily return results as string from GetCommitMessages() until we
    implement another function to handle user output control, at which time
    GetCommitMessages() should just return JSON so it can be better used in
    other modules if desired.

--- main.go
+++ main.go
@@ -2,15 +2,17 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"log"
 
 	"github.com/google/go-github/v39/github"
 )
 
-// Comparison specifies the parameters required for commit comparison requests against
+// Request specifies the parameters required for commit comparison requests against
 // GitHub, as well as any additional formatting/output control settings
-type Comparison struct {
+type Request struct {
 	owner string
 	repo  string
 	head  string
@@ -20,38 +22,72 @@ type Comparison struct {
 	short bool
 }
 
+// CommitMessage specifies the message content, author, and associated git commit
+// sha of commits to be fetched in the comparison.
+type CommitMessage struct {
+	Author    *github.CommitAuthor `json:"author,omitempty"`
+	CommitSHA *string              `json:"sha,omitempty"`
+	Message   *string              `json:"message,omitempty"`
+}
+
 // GetCommitMessages calls the GitHub API for comparison of commits between specified BASE and HEAD
 // It returns commit messages, SHAs, and author info as a byte slice
-func GetCommitMessages(c *Comparison) (string, error) {
+func GetCommitMessages(r *Request) (string, error) {
 	//func GetCommitMessages(c *Comparison) ([]byte, error) {
 
 	ctx := context.Background()
 
 	tp := github.BasicAuthTransport{
-		Username: c.user,
-		Password: c.pass,
+		Username: r.user,
+		Password: r.pass,
 	}
 
-	client = github.NewClient(tp.Client())
+	client := github.NewClient(tp.Client())
 
-	listOptions := &github.ListOptions{1, 100}
+	var res []CommitMessage
 
-	r, _, err := client.Repositories.CompareCommits(ctx, c.owner, c.repo, c.base, c.head, listOptions)
+	for page, numCommits, firstRun := 1, 1, true; numCommits > 0; page++ {
+		options := &github.ListOptions{page, 100}
+		c, _, err := client.Repositories.CompareCommits(ctx, r.owner, r.repo, r.base, r.head, options)
+		if err != nil {
+			return "Error", err
+		}
+
+		for i, l := 0, len(c.Commits); i < l-1; i++ {
+			currentCommit := c.Commits[i].Commit
+
+			m := CommitMessage{
+				Author:    currentCommit.Author,
+				CommitSHA: currentCommit.SHA,
+				Message:   currentCommit.Message,
+			}
+
+			res = append(res, m)
+		}
+
+		if firstRun == true {
+			numCommits = *c.TotalCommits
+
+			firstRun = false
+		}
+
+		numCommits = numCommits - 100
+	}
+
+	if r.short {
+		fmt.Println("Short input was requested")
+	}
+
+	resJSON, err := json.MarshalIndent(res, "", " ")
 	if err != nil {
 		return "Error", err
 	}
 
-	if c.short {
-		fmt.Println("Short input was requested")
-	}
-
-	fmt.Println(r)
-
-	return "Test", nil
+	return string(resJSON), nil
 }
 
 func main() {
-	c := new(Comparison)
+	c := new(Request)
 	flag.StringVar(&c.owner, "owner", "", "Name of the user or organization the repo belongs to")
 	flag.StringVar(&c.repo, "repo", "", "Name of the repo to compare commits from")
 	flag.StringVar(&c.head, "head", "", "Git commit SHA, branch, or tag of commit to use as HEAD in the comparison")
@@ -61,5 +97,10 @@ func main() {
 	flag.BoolVar(&c.short, "short", false, "Minimal output with messages only (omit author and commit info)")
 	flag.Parse()
 
-	GetCommitMessages(c)
+	res, err := GetCommitMessages(c)
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	fmt.Println(res)
 }
